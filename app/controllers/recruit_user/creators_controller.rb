02d33056f1406b84783d3ba01a47e5bd75c4ab50
AMI-KATSUMATA <amic2264@gmail.com>
2021-09-07 15:01:11 +0000

    [add]creator-index/show

--- app/controllers/recruit_user/creators_controller.rb
+++ app/controllers/recruit_user/creators_controller.rb
@@ -1,2 +1,10 @@
 class RecruitUser::CreatorsController < ApplicationController
+  def index
+    @creators = Creator.where(is_deleted: "false")
+  end
+
+  def show
+    @creator = Creator.find(params[:id])
+    # @artworks = @creator.artworks.page(params[:page]).reverse_order
+  end
 end
