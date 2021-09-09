33fc19e591581f1a494c2e318ad88e94b6b05e58
AMI-KATSUMATA <amic2264@gmail.com>
2021-09-09 10:36:47 +0000

    [fix]

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,6 @@
 class Favorite < ApplicationRecord
-  
+
   belongs_to :creator
-  belongs_to :musician
-  belongs_to :recruitment
+  belongs_to :musician, optional: true
+  belongs_to :recruitment, optional: true
 end
